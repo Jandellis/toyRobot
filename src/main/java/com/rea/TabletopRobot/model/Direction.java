0791ae0e26c678ec49b3e98624fcc2ad2bdcdf5d
James McNamara <james.mcnamara@hotmail.com.au>
2017-09-03 09:24:38 +1000

    Added some models

---
+++ src/main/java/com/rea/TabletopRobot/model/Direction.java
@@ -0,0 +1,11 @@
+package com.rea.TabletopRobot.model;
+
+/**
+ * Created by james_000 on 3/09/2017.
+ */
+public enum Direction {
+    NORTH,
+    EAST,
+    SOUTH,
+    WEST;
+}
---
+++ src/main/java/com/rea/TabletopRobot/model/Robot.java
@@ -0,0 +1,9 @@
+package com.rea.TabletopRobot.model;
+
+/**
+ * Created by james_000 on 3/09/2017.
+ */
+public class Robot {
+    private Location location;
+    private boolean placed = false;
+}
