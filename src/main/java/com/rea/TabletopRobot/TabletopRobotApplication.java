86d091cf96a957272fdbc02c7bcce9071e600580
James McNamara <james.mcnamara@hotmail.com.au>
2017-09-03 09:16:21 +1000

    First commit

---
+++ src/main/java/com/rea/TabletopRobot/TabletopRobotApplication.java
@@ -0,0 +1,12 @@
+package com.rea.TabletopRobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TabletopRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TabletopRobotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rea/TabletopRobot/TabletopRobotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rea.TabletopRobot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TabletopRobotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
