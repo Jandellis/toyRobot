311cee0f5a571b8157ad266cb1c4b2a2ef528552
James McNamara <james.mcnamara@hotmail.com.au>
2017-09-03 10:06:17 +1000

    Add more tests

--- src/test/java/com/rea/TabletopRobot/controler/TabletopTest.java
+++ src/test/java/com/rea/TabletopRobot/controler/TabletopTest.java
@@ -23,8 +23,32 @@ public class TabletopTest {
   }
 
   @Test
-  public void placeValidRobot() throws Exception {
+  public void placeValidRobotSouthWestCorner() throws Exception {
     Location location = new Location(0,0, Direction.NORTH);
+    Tabletop tabletop = new Tabletop();
+    boolean result = tabletop.placeRobot(location);
+    assertThat(result, is(true));
+  }
+
+  @Test
+  public void placeValidRobotSouthEastCorner() throws Exception {
+    Location location = new Location(4,0, Direction.EAST);
+    Tabletop tabletop = new Tabletop();
+    boolean result = tabletop.placeRobot(location);
+    assertThat(result, is(true));
+  }
+
+  @Test
+  public void placeValidRobotNorthWestCorner() throws Exception {
+    Location location = new Location(0,4, Direction.WEST);
+    Tabletop tabletop = new Tabletop();
+    boolean result = tabletop.placeRobot(location);
+    assertThat(result, is(true));
+  }
+
+  @Test
+  public void placeValidRobotNorthEastCorner() throws Exception {
+    Location location = new Location(4,4, Direction.SOUTH);
     Tabletop tabletop = new Tabletop();
     boolean result = tabletop.placeRobot(location);
     assertThat(result, is(true));
@@ -41,6 +65,39 @@ public class TabletopTest {
   @Test
   public void placeInvalidRobotYLow() throws Exception {
     Location location = new Location(0,-1, Direction.NORTH);
+    Tabletop tabletop = new Tabletop();
+    boolean result = tabletop.placeRobot(location);
+    assertThat(result, is(false));
+  }
+
+  @Test
+  public void placeInvalidRobotXHigh() throws Exception {
+    Location location = new Location(5,2, Direction.NORTH);
+    Tabletop tabletop = new Tabletop();
+    boolean result = tabletop.placeRobot(location);
+    assertThat(result, is(false));
+  }
+
+  @Test
+  public void placeInvalidRobotXLow() throws Exception {
+    Location location = new Location(-1,3, Direction.NORTH);
+    Tabletop tabletop = new Tabletop();
+    boolean result = tabletop.placeRobot(location);
+    assertThat(result, is(false));
+  }
+
+
+  @Test
+  public void placeInvalidRobotBothHigh() throws Exception {
+    Location location = new Location(5,20, Direction.NORTH);
+    Tabletop tabletop = new Tabletop();
+    boolean result = tabletop.placeRobot(location);
+    assertThat(result, is(false));
+  }
+
+  @Test
+  public void placeInvalidRobotBothLow() throws Exception {
+    Location location = new Location(-1,-3, Direction.NORTH);
     Tabletop tabletop = new Tabletop();
     boolean result = tabletop.placeRobot(location);
     assertThat(result, is(false));
